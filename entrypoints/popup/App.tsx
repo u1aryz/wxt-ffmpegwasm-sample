4cc3ab1fecfd5beac2bb91c66ec7552f4fad80c7
u1aryz <u1aryz.d@gmail.com>
2025-10-06 01:37:41 +0900

    feat: display ffmpeg version on load

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,7 +1,7 @@
 import { FFmpeg } from "@ffmpeg/ffmpeg";
 
 function App() {
-	const [loaded, setLoaded] = useState(false);
+	const [messages, setMessages] = useState<string[]>([]);
 	const ffmpegRef = useRef(new FFmpeg());
 
 	const load = async () => {
@@ -9,17 +9,28 @@ function App() {
 		const coreURL = chrome.runtime.getURL("/ffmpeg/ffmpeg-core.js");
 		const wasmURL = chrome.runtime.getURL("/ffmpeg/ffmpeg-core.wasm");
 
+		ffmpeg.on("log", ({ message, type }) => {
+			if (type === "stdout") {
+				setMessages((prevMessages) => [...prevMessages, message]);
+			}
+		});
 		await ffmpeg.load({
 			coreURL,
 			wasmURL,
 		});
-		setLoaded(true);
+
+		await ffmpeg.exec(["-version"]);
 	};
 
 	return (
 		<div className="p-4">
-			{loaded ? (
-				"Loaded"
+			{messages.length > 0 ? (
+				<div>
+					<h3 className="font-bold text-lg">Loaded</h3>
+					{messages.map((message, index) => (
+						<p key={index}>{message}</p>
+					))}
+				</div>
 			) : (
 				<button
 					className="rounded-lg bg-blue-600 px-6 py-2 font-semibold text-white transition hover:bg-blue-700 disabled:cursor-not-allowed disabled:bg-gray-400"
